7265adcb3b83cf62fb6710c4a570a55d2840738e
Anton Aroche <anton.aroche@gmail.com>
2020-03-02 22:19:14 -0500

    Changed images of Pokecards and improved styling

--- pokegame/src/Pokecard.js
+++ pokegame/src/Pokecard.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
 
+/**
+ * Gets a pretty image source directly from Pokemon's assets.
+ * Has to be padded with zeros to 3 places.
+ * @param {*} id 
+ */
+function generateSource(id) {
+    let convertedId
+    if (id < 10) {
+        convertedId = `00${id}`
+    }
+    else {
+        convertedId = (id < 100) ? `0${id}` : id
+    }
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${convertedId}.png`
+}
+
+
 class Pokecard extends Component {
   render() {
     const { name, id, type, base_experience } = this.props;
-    const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-    console.log(imageSrc)
 
     return (
       <div className="Pokecard">
+        <img className="Pokecard-image" src={generateSource(id)} alt={name}/>
         <p className="Pokecard-name">{name}</p>
-        <img className="Pokecard-image" src={imageSrc} alt={name}/>
         <p className="Pokecard-type"> Type: {type}</p>
         <p className="Pokecard-exp">EXP: {base_experience}</p>
       </div>
