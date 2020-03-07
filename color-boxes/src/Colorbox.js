26309162ec5855ea5813b9ce4dc445b794f5f9fd
Anton Aroche <anton.aroche@gmail.com>
2020-03-07 11:34:14 -0500

    Refactored color-boxes to have downward data flow

--- color-boxes/src/Colorbox.js
+++ color-boxes/src/Colorbox.js
@@ -1,32 +1,24 @@
-import React, { Component } from 'react';
-import './Colorbox.css';
-
-const colours = [
-    'red', 'blue', 'green', 'teal', 'orange', 'purple', 'slateblue', 'lightgreen'
-]
-
-function randomColor() {
-    return colours[(Math.floor(Math.random() * colours.length))]
-}
+import React, { Component } from "react";
+import "./Colorbox.css";
 
 class Colorbox extends Component {
-    state = { color: randomColor() }
+  static defaultProps = {
+    color: "purple"
+  };
 
-    setRandomColour = e => {
-        let newColor
-        do {
-            newColor = randomColor() 
-        } while (newColor === this.state.color)
-        
-        this.setState({color: newColor})
-    }
+  handleChangeColor = () => {
+    this.props.changeColor(this.props.index);
+  };
 
-    render() { 
-        return ( 
-            <div className="Colorbox" onClick={this.setRandomColour} style={{backgroundColor: this.state.color}}>
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div
+        className="Colorbox"
+        onClick={this.handleChangeColor}
+        style={{ backgroundColor: this.props.color }}
+      />
+    );
+  }
 }
- 
+
 export default Colorbox;
--- color-boxes/src/ColorboxContainer.js
+++ color-boxes/src/ColorboxContainer.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react';
-import Colorbox from './Colorbox';
-import './ColorboxContainer.css'
+import React, { Component } from "react";
+import Colorbox from "./Colorbox";
+import "./ColorboxContainer.css";
+
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "teal",
+  "orange",
+  "purple",
+  "slateblue",
+  "lightgreen"
+];
+
+function randomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
 class ColorboxContainer extends Component {
-    static defaultProps = {
-        numBoxes: 18
-    }
-    state = {  }
+  static defaultProps = {
+    numBoxes: 18
+  };
 
-    render() { 
-        const boxes = Array.from({length: this.props.numBoxes}).map(() => (
-            <Colorbox />
-        ))
+  state = {
+    colors: Array.from({ length: this.props.numBoxes }).map(() => randomColor())
+  };
 
-        return ( 
-            <div className="ColorboxContainer">
-                {boxes}
-            </div>
-         );
-    }
+  changeColor = index => {
+    this.setState({
+      colors: this.state.colors.map((c, i) => {
+        if (i === index) {
+          c = randomColor();
+        }
+        return c;
+      })
+    });
+  };
+
+  render() {
+    return (
+      <div className="ColorboxContainer">
+        {this.state.colors.map((c, i) => (
+          <Colorbox color={c} index={i} changeColor={this.changeColor} />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default ColorboxContainer;
