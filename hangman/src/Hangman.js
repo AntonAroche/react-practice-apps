92d0f3533d7460bf9f10de4d356b7ce6ae0d73e1
Anton Aroche <anton.aroche@gmail.com>
2020-03-08 14:38:24 -0400

    Added number of wrong guesses and losing handling oto Hangman

--- hangman/src/Hangman.js
+++ hangman/src/Hangman.js
@@ -46,6 +46,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -58,11 +59,18 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     return (
-      <div className='Hangman'>
+      <div className="Hangman">
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <img src={this.props.images[this.state.nWrong]} alt={this.state.nWrong + " wrong guesses"}/>
+        <p className="Hangman-wrong">Number wrong:{this.state.nWrong}</p>
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        {this.props.maxWrong === this.state.nWrong ? (
+          <p className="Hangman-lost">
+            You lose! The word was {this.state.answer}.
+          </p>
+        ) : (
+          <p className="Hangman-btns">{this.generateButtons()}</p>
+        )}
       </div>
     );
   }
