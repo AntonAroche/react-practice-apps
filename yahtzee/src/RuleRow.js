c790319d668c2b183ea5521587d0a0f3885af89f
Anton Aroche <anton.aroche@gmail.com>
2020-03-26 20:39:40 -0400

    Rules are now properly disabled when they are used

--- yahtzee/src/RuleRow.js
+++ yahtzee/src/RuleRow.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
+    let score = this.props.score;
+    let rowType;
+
+    if (score) {
+      rowType = "RuleRow-disabled";
+    } else {
+      rowType = "RuleRow-active";
+    }
+
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr
+        className={`RuleRow ${rowType}`}
+        onClick={score ? undefined : this.props.doScore}
+      >
         <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
-    )
+    );
   }
 }
 
