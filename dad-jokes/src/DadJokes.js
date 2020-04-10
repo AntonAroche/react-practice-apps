5f283cd05eca83d5543eb5330e8a43be0f11af7f
Anton Aroche <anton.aroche@gmail.com>
2020-04-09 23:11:25 -0400

    Duplicate jokes are no longer encountered

--- dad-jokes/src/DadJokes.js
+++ dad-jokes/src/DadJokes.js
@@ -6,18 +6,19 @@ import "./DadJokes.css";
 const jokeUrl = "https://icanhazdadjoke.com/";
 
 class DadJokes extends Component {
-  constructor(props) {
-    super(props);
-    this.getJokes = this.getJokes.bind(this);
-  }
-
   static defaultProps = {
     numJokesToGet: 10,
   };
 
-  state = {
-    jokes: JSON.parse(window.localStorage.getItem("jokes") || []),
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+    };
+    this.seenJokes = new Set(this.state.jokes.map((j) => j.joke));
+    this.getJokes = this.getJokes.bind(this);
+  }
 
   componentDidMount() {
     if (this.state.jokes.length === 0) {
@@ -31,20 +32,25 @@ class DadJokes extends Component {
       let response = await axios.get(jokeUrl, {
         headers: { Accept: "application/json" },
       });
-      retrievedJokes.push({
-        joke: response.data.joke,
-        id: response.data.id,
-        votes: 0,
-      });
+      if (!this.seenJokes.has(response.data.joke)) {
+        this.seenJokes.add(response.data.joke)
+        retrievedJokes.push({
+          joke: response.data.joke,
+          id: response.data.id,
+          votes: 0,
+        });
+      }
     }
     this.setState(
       (st) => ({
+        loading: false,
         jokes: [...st.jokes, ...retrievedJokes],
       }),
       () =>
         window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
     );
   }
+  s;
 
   handleVote = (jokeId, delta) => {
     this.setState(
@@ -58,8 +64,21 @@ class DadJokes extends Component {
     );
   };
 
+  handleClickNewJokes = () => {
+    this.setState({ loading: true }, this.getJokes);
+  };
+
   render() {
-    const jokes = this.state.jokes.map((j) => (
+    const sortedJokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
+    if (this.state.loading) {
+      return (
+        <div className="DadJokes-spinner">
+          <i className="far fa-8x fa-laugh fa-spin" />
+          <h1 className="DadJokes-loading DadJokes-title">Loading...</h1>
+        </div>
+      );
+    }
+    const jokes = sortedJokes.map((j) => (
       <Joke
         jokeText={j.joke}
         key={j.id}
@@ -79,7 +98,7 @@ class DadJokes extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt="laughing-emoji"
           />
-          <button className="DadJokes-btn" onClick={this.getJokes}>
+          <button className="DadJokes-btn" onClick={this.handleClickNewJokes}>
             New Jokes
           </button>
         </div>
