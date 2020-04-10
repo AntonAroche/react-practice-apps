60df38e5cb654c0e6712543be846c09168b0672a
Anton Aroche <anton.aroche@gmail.com>
2020-04-10 13:03:42 -0400

    Added react router and Dog component, displaying the details of each dog.

--- dog-finder/src/App.js
+++ dog-finder/src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import whiskey from "./images/whiskey.jpg";
+import hazel from "./images/hazel.jpg";
+import tubby from "./images/tubby.jpg";
+import ollie from "./images/ollie.jpeg";
+import Dog from './Dog.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!",
+        ],
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs.",
+        ],
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food.",
+        ],
+      },
+      {
+        name: "Ollie",
+        age: 10,
+        src: ollie,
+        facts: [
+          "Ollie has mastered many tricks.",
+          "Ollie loves to take naps.",
+          "Ollie is always happy to see you!",
+        ],
+      },
+    ],
+  };
+
+  render() {
+    const dogRoutes = this.props.dogs.map((d) => {
+      return <Route path={`/dogs/${d.name}`} render={() => <Dog dog={d}/>} />;
+    });
+
+    return (
+      <div className="App">
+        <Switch>
+          {dogRoutes}
+          <Route path="/dogs" render={() => <h1>Dogs!</h1>} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ dog-finder/src/Dog.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Dog extends Component {
+    render() { 
+        const {name, age, src, facts} = this.props.dog
+        const renderFacts = facts.map((f) => {
+            return (
+                <div className="Dog-facts">
+                    <h1>{f}</h1>
+                </div>
+            )
+        })
+
+        return (
+            <div className="Dog">
+                <h1 className="Dog-name">{name}</h1>
+                <h2 className="Dog-age">Age: {age}</h2>
+                <img src={src}/>
+                {renderFacts}
+            </div>
+        );
+    }
+}
+ 
+export default Dog;
